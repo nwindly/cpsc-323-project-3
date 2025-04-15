ea0303ca7238322511ba840122839906ffcbde52
kalmk <111930167+kalmk@users.noreply.github.com>
2025-04-14 19:33:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from tools.parser.parser_main import parser
 def main():
     token_stream = lexer("./test_cases/1.in")
 
-    # Creates input.in file for the parser to read
+    # Test
     parser_input = ' '.join(token_stream)
     with open('parser_input.in', 'w') as file:
         file.write(parser_input)
-
     # Change this function below to accept not from .in file but directly from token_stream (array)
     parser("./parser_input.in")
 
