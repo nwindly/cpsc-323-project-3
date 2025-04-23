2dee32c39e8f7aee7365ebb8da7ddc3066dc6a34
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-23 14:39:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,10 @@ def main():
     token_stream = lexer("./test_cases/1.in")
     tree = parser(token_stream)
     nodes = tree.get_all_nodes()
-
-    # IMPORTANT: element of nodes is a tree object. When using Print(element), it will print the value. However,
-    # the value's type is NOT string. It is a tree object type. Therefore, we need to typecast it to String everytime
-    # we want to do any calculations and comparisons.
-    #
-    #    EXAMPLE:
-    #
-    #   x = nodes[0]
-    #   print("P"== str(x))
-    #   print(type(x))
-    #   print(x)
-    #
-    #   a = nodes[7] + "sh"
-    #   TypeError: unsupported operand type(s) for +: 'tree_node' and 'str'
-
     index = 0
-    for i in nodes:
-        print(f"{index}: {i}")
+    for node in nodes:
+        print(f"{index}: {node}")
         index = index + 1
-
-    x = nodes[7] + "sh"
-    print(x)
 
 
 
