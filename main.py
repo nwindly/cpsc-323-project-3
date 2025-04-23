2c57ef1d769744571a6ff75cb09afeadb2f49cd1
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-23 14:22:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from tools.lexer.lexer_main import lexer
 from tools.parser.parser_main import *
 
+
 def main():
     token_stream = lexer("./test_cases/1.in")
-
     tree = parser(token_stream)
     nodes = tree.get_all_nodes()
+    index = 0
     for i in nodes:
-        print(i)
+        print(f"{index}: {i}")
+        index = index + 1
 
 if __name__ == "__main__":
     main()
