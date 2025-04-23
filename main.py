649a40cd2bb972a0a0d59d26e20a78e961964816
robinwinter1 <111930167+kalmk@users.noreply.github.com>
2025-04-23 14:33:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,24 @@ def main():
     token_stream = lexer("./test_cases/1.in")
     tree = parser(token_stream)
     nodes = tree.get_all_nodes()
+
+    # IMPORTANT: element of nodes is a tree object. When using Print(element), it will print the value. However,
+    # the value's type is NOT string. It is a tree object type. Therefore, we need to typecast it to String everytime
+    # we want to do any calculations and comparisons.
+    # EXAMPLE:
+    # x = nodes[0]
+    # print("P"== str(x))
+    # print(type(x))
+    # print(x)
+
     index = 0
     for i in nodes:
         print(f"{index}: {i}")
         index = index + 1
+
+
+
+    
 
 if __name__ == "__main__":
     main()
